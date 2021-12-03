590c0c9694b6a5594eb6a359cebc71ad273bbeaa
DuyTruong159 <73580494+DuyTruong159@users.noreply.github.com>
2021-12-04 01:04:03 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', ['as'=>'home', 'uses'=>'TagController@index']);
+Route::get('/blog-detail/{blogId}', ['as'=>'blogDetail', 'uses'=>'BlogController@blogDetail']);
+Route::get('/blog-post', ['as'=>'blogPost', 'uses'=>'TagController@blogPost']);
+
+Route::post('/blog-post', ['as'=>'blogpostdone', 'uses'=>'BlogController@insert']);
+
+Route::view('/register', 'register');
